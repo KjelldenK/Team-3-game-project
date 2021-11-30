dcc3423d85db67828ebd0561ebcffea8cd56249f
kjg1993 <kguzman077@gmail.com>
2021-11-29 22:15:40 -0500

    Pull request from the week 12

--- project/enemies.py
+++ project/enemies.py
@@ -1,10 +1,17 @@
 import arcade
 import random
+from  ship_live import ShipSprite 
 
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Enemies"
+ENEMY_SPEED = 2
+ENEMY_VERTICAL_MARGIN = 15
+RIGHT_ENEMY_BORDER = SCREEN_WIDTH - ENEMY_VERTICAL_MARGIN
+LEFT_ENEMY_BORDER = ENEMY_VERTICAL_MARGIN
+ENEMY_MOVE_DOWN_AMOUNT = 30 
+SCALE = 0.75
 
 
 class Enemies(arcade.Window):
@@ -19,14 +26,50 @@ class Enemies(arcade.Window):
         self.enemy_list = []
         self.bullet_list = []
         
+         # Enemy movement
+        self.enemy_change_x = -ENEMY_SPEED
+
+        # Textures for the enemy
+        self.enemy_textures = None
+
+         # Set up Player Lives 
+        self.player_sprite = None
+        self.lives = 3
+        
 
     def setup(self):
         """ Setup the variables for the game. """
         # self.player_list = arcade.SpriteList()
+        self.player_sprite_list = arcade.SpriteList()
         self.enemy_list = arcade.SpriteList()
         self.bullet_list = arcade.SpriteList()
-        
+        self.show_lives = arcade.SpriteList()
+        self.ship_life_list = arcade.SpriteList()
 
+        self.player_sprite = ShipSprite(":resources:images/space_shooter/"
+                                        "playerShip1_orange.png",
+                                        SCALE)
+        self.player_sprite_list.append(self.player_sprite)
+        self.lives = 4
+
+        # Set up the little icons that represent the player lives.
+        cur_pos = 10
+        for i in range(self.lives):
+            life = arcade.Sprite(":resources:images/space_shooter/"
+                                 "playerLife1_blue.png",
+                                 SCALE)
+            life.center_x = cur_pos + life.width
+            life.center_y = life.height
+            cur_pos += life.width
+            self.ship_life_list.append(life)
+
+
+# Load the textures for the enemies, one facing left, one right  :resources:images/space_shooter/playerShip1_orange.png", 0.5
+        self.enemy_textures = []
+        texture = arcade.load_texture(":resources:images/space_shooter/playerShip1_blue.png", mirrored=True)
+        self.enemy_textures.append(texture)
+        texture = arcade.load_texture(":resources:images/space_shooter/playerShip1_orange.png", mirrored=True)
+        self.enemy_textures.append(texture)
        
         # Add top-left enemy ship
         enemy = arcade.Sprite(":resources:images/space_shooter/playerShip1_blue.png", 0.5)
@@ -39,7 +82,7 @@ class Enemies(arcade.Window):
 
  
         # Add top-right enemy ship
-        enemy = arcade.Sprite(":resources:images/space_shooter/playerShip1_orange.png", 0.5)
+        enemy = arcade.Sprite(":resources:images/space_shooter/playerShip1_blue.png", 0.5)
         enemy.right = 0.3
         enemy.center_x = SCREEN_WIDTH - 120
         enemy.center_y = SCREEN_HEIGHT - enemy.height
@@ -55,6 +98,9 @@ class Enemies(arcade.Window):
 
         self.enemy_list.draw()
         self.bullet_list.draw()
+        #lives
+        self.show_lives.draw()
+        self.ship_life_list.draw()
 
     def on_update(self, time):
         """All the logic to move, and the game logic goes here. """
@@ -82,14 +128,35 @@ class Enemies(arcade.Window):
         self.bullet_list.update()
 
         #set move
-        if random.random() < 0.9:
-            enemy.left = SCREEN_HEIGHT - enemy.height
+        for enemy in self.enemy_list:
+            enemy.center_x += self.enemy_change_x
+
+             
+        # Check every enemy to see if any hit the edge. If so, reverse the
+        # direction and flag to move down.
+        move_down = False
+        for enemy in self.enemy_list:
+            if enemy.right > RIGHT_ENEMY_BORDER and self.enemy_change_x > 0:
+                self.enemy_change_x *= -1
+                move_down = True
+            if enemy.left < LEFT_ENEMY_BORDER and self.enemy_change_x < 0:
+                self.enemy_change_x *= -1
+                move_down = True
+
+        # Did we hit the edge above, and need to move t he enemy down?
+        if move_down:
+            # Yes
             for enemy in self.enemy_list:
-                enemy.center_x -= 1 # set the velocity
-                if enemy.center_x <= 775:
-                    for enemy in self.enemy_list:
-                        enemy.center_x += 1
-                        
+                # Move enemy down
+                enemy.center_y -= ENEMY_MOVE_DOWN_AMOUNT
+                # Flip texture on enemy so it faces the other way
+                if self.enemy_change_x > 0:
+                    enemy.texture = self.enemy_textures[0]
+                # else:
+                #     enemy.texture = self.enemy_textures[1]      
+
+              
+
 
 def main():
     """ Main function """
@@ -100,4 +167,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
 
